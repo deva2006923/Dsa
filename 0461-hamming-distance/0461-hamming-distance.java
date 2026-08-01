f1788d7c1565a9c355567d843c7310533dfe439f
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-08-01 10:38:24 +0530

    Time: 0 ms (100%), Space: 42.2 MB (59.29%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,6 +1,20 @@
 class Solution {
     public int hammingDistance(int x, int y) {
+        int count=0;
+        while(x!=0||y!=0){
+            if((x&1)!=(y&1)){
+                count++;
+            }
+            x=x>>1;
+            y=y>>1;
+        }
+        return count;
         
-        return Integer.bitCount(x^y);
     }
 }
+
+
+/*
+ONE LINE ANS :
+return Integer.bitCount(x^y);
+*/ 
