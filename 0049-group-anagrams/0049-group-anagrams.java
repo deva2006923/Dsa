dca7babf2929d67920feecf06461c9f2b3ff4cd5
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-08-01 14:42:35 +0530

    Time: 8 ms (34.65%), Space: 50.1 MB (20.71%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(String s:strs){
+            
+            char[] ch=s.toCharArray();
+            Arrays.sort(ch);
+            String key=new String(ch);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        List<List<String>> res=new ArrayList<>();
+        for(String key:map.keySet()){
+            res.add(map.get(key));
+        }
+        return res;
+        
+    }
+}
