93ab44b4df9b61102a4c408002f0500d3a6c94f8
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-08-01 16:04:38 +0530

    Time: 35 ms (66.42%), Space: 14.6 MB (72.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        d={}
+        d={0:1}
         total=0
         c=0
         for i in nums:
             total=total+i
             if((total-k) in d):
-                c=c+d[i]
+                c=c+d[total-k]
             d[total]=d.get(total,0)+1
         return c;        
 
