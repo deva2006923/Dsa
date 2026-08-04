56e70c992a2aaae6531571a7e51fe09e06610e9b
DEVA PRAKASSH M J <devaprakassh49@gmail.com>
2026-08-04 10:24:09 +0530

    Create code.java

---
+++ permutation/code.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>> ans=new ArrayList<>();
+    public List<List<Integer>> permuteUnique(int[] nums){
+        boolean[] v=new boolean[nums.length];
+        bt(nums,v,new ArrayList<>());
+        return ans;
+        
+    }
+    public void bt(int[] nums,boolean[] v,List<Integer> temp){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(v[i]){
+                continue;
+            }
+            if(i>0&&nums[i]==nums[i-1]&&!v[i-1]){
+                continue;
+            }
+            v[i]=true;
+            temp.add(nums[i]);
+            bt(nums,v,temp);
+            v[i]=false;
+            temp.remove(temp.size()-1);
+        }
+    }
+}
